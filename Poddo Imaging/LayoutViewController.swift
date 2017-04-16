a93c8c761f08b41db1d8c55668021031c2bcf6ac
Mayank Sanganeria <mayanks@uber.com>
2017-04-16 13:32:57 +0530

    drag to position + draw offscreen

--- Poddo Imaging/LayoutViewController.swift
+++ Poddo Imaging/LayoutViewController.swift
@@ -8,13 +8,7 @@
 
 import UIKit
 import SnapKit
-
-enum LayoutViewControllerPosition {
-    case topLeft
-    case topRight
-    case bottomLeft
-    case bottomRight
-}
+import UIView_draggable
 
 class LayoutViewController: UIViewController {
 
@@ -23,13 +17,11 @@ class LayoutViewController: UIViewController {
 
     @IBOutlet weak var paintingImageView: UIImageView!
     @IBOutlet weak var artistImageView: UIImageView!
-    var artistSize = CGFloat(100.0)
-    var position = LayoutViewControllerPosition.topLeft
-
     @IBOutlet weak var compositeView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintingImageView.contentMode = .scaleAspectFit
         paintingImageView.image = paintingImage
         artistImageView.image = artistImage
 
@@ -38,70 +30,72 @@ class LayoutViewController: UIViewController {
                 make.height.equalTo(paintingImageView.snp.width).multipliedBy(paintingImage.size.height / paintingImage.size.width)
             }
         }
+        artistImageView.isUserInteractionEnabled = true
+        artistImageView.enableDragging()
     }
 
     @IBAction func saveImage(_ sender: Any) {
-        UIGraphicsBeginImageContextWithOptions(compositeView.bounds.size, compositeView.isOpaque, 0.0)
-        compositeView.drawHierarchy(in: compositeView.bounds, afterScreenUpdates: false)
-        let snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext()
-        UIImageWriteToSavedPhotosAlbum(snapshotImageFromMyView!, nil,nil, nil);
-        UIGraphicsEndImageContext()
+        saveToPhotos(view: offscreenImageView())
+        messageDone()
+    }
 
+    @IBAction func changeSize(_ sender: UISlider) {
+        setArtistSizePercentage(CGFloat(sender.value))
+    }
+
+    private func setArtistSizePercentage(_ percentage: CGFloat) {
+        let artistSize = min(paintingImageView.bounds.size.width, paintingImageView.bounds.size.height) * percentage
+        print(artistSize)
+        artistImageView.bounds = CGRect(x: 0, y: 0, width: artistSize, height: artistSize)
+    }
+
+    private func offscreenImageView() -> UIView? {
+        if let paintingImage = paintingImage, let artistImage = artistImage {
+            let container = UIImageView(frame: CGRect(x: 0, y: 0, width: paintingImage.size.width, height: paintingImage.size.height))
+            container.backgroundColor = .black
+            container.image = paintingImage
+            let maxDimension = max(paintingImage.size.width, paintingImage.size.height)
+            let artistSize = artistImageView.bounds.size.width / paintingImageView.bounds.size.width * maxDimension
+            var artistX = CGFloat(0.0)
+            var artistY = CGFloat(0.0)
+
+            let artistXInitial = artistImageView.frame.origin.x + artistImageView.bounds.size.width/2.0 - paintingImageView.bounds.size.width/2.0
+            let artistYInitial = artistImageView.frame.origin.y + artistImageView.bounds.size.height/2.0 - paintingImageView.bounds.size.height/2.0
+            if paintingImage.size.width > paintingImage.size.height {
+                artistX = artistXInitial / paintingImageView.bounds.size.width * paintingImage.size.width
+                artistY = artistYInitial / paintingImageView.bounds.size.width * paintingImage.size.width
+            } else if paintingImage.size.width < paintingImage.size.height {
+                artistX = artistXInitial / paintingImageView.bounds.size.height * paintingImage.size.height
+                artistY = artistYInitial / paintingImageView.bounds.size.height * paintingImage.size.height
+            } else if paintingImage.size.width == paintingImage.size.height {
+                artistX = artistXInitial / paintingImageView.bounds.size.width * paintingImage.size.width
+                artistY = artistYInitial / paintingImageView.bounds.size.height * paintingImage.size.height
+            }
+
+            artistX = artistX + paintingImage.size.width/2.0 - artistSize/2.0
+            artistY = artistY + paintingImage.size.height/2.0 - artistSize/2.0
+
+            let artistView = UIImageView(frame: CGRect(x: artistX, y: artistY, width: artistSize, height: artistSize))
+            artistView.image = artistImage
+            container.addSubview(artistView)
+            return container
+        }
+        return nil
+    }
+
+    private func messageDone() {
         let alert = UIAlertController(title: "Saved", message: "Saved in Photos", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 
-    @IBAction func changeSize(_ sender: UISlider) {
-        artistSize = min(paintingImageView.bounds.size.width, paintingImageView.bounds.size.height) * CGFloat(sender.value)
-        updateArtistConstraints()
-    }
-
-    @IBAction func topLeft(_ sender: Any) {
-        artistImageView.snp.remakeConstraints({ (make) in
-            position = .topLeft
-            updateArtistConstraints()
-        })
-    }
-    @IBAction func topRight(_ sender: Any) {
-        artistImageView.snp.remakeConstraints({ (make) in
-            position = .topRight
-            updateArtistConstraints()
-        })
-    }
-    @IBAction func bottomLeft(_ sender: Any) {
-        artistImageView.snp.remakeConstraints({ (make) in
-            position = .bottomLeft
-            updateArtistConstraints()
-        })
-    }
-    @IBAction func bottomRight(_ sender: Any) {
-        position = .bottomRight
-        updateArtistConstraints()
-    }
-
-    private func updateArtistConstraints() {
-        artistImageView.snp.remakeConstraints({ (make) in
-            make.height.width.equalTo(artistSize)
-            let padding = 10
-            switch position {
-            case .topLeft:
-                make.top.equalTo(paintingImageView).offset(padding)
-                make.left.equalTo(paintingImageView).offset(padding)
-                break
-            case .topRight:
-                make.top.equalTo(paintingImageView).offset(padding)
-                make.right.equalTo(paintingImageView).offset(-padding)
-                break
-            case .bottomLeft:
-                make.bottom.equalTo(paintingImageView).offset(-padding)
-                make.left.equalTo(paintingImageView).offset(padding)
-                break
-            case .bottomRight:
-                make.bottom.equalTo(paintingImageView).offset(-padding)
-                make.right.equalTo(paintingImageView).offset(-padding)
-                break
-            }
-        })
+    private func saveToPhotos(view: UIView?) {
+        if let view = view {
+            UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0)
+            view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+            let snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext()
+            UIImageWriteToSavedPhotosAlbum(snapshotImageFromMyView!, nil,nil, nil);
+            UIGraphicsEndImageContext()
+        }
     }
 }
